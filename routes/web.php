7317f12ae9c78e891cd65c1c7ef9af806c9e35ac
Pavel Ivanov <p.ivanov.spb@gmail.com>
2020-02-08 22:42:58 +0300

    added ManufacturerManagementTest

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ Route::post('/spareparts', 'SparepartController@store');
 Route::patch('/spareparts/{sparepart}', 'SparepartController@update');
 //Route::patch('/spareparts/{sparepart}-{slug}', 'SparepartController@update');
 Route::delete('/spareparts/{sparepart}', 'SparepartController@destroy');
+
+Route::post('/manufacturers', 'ManufacturerController@store');
--- tests/Feature/ManufacturerManagementTest.php
+++ tests/Feature/ManufacturerManagementTest.php
@@ -2,21 +2,30 @@
 
 namespace Tests\Feature;
 
+use App\Entities\Manufacturer;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ManufacturerManagementTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function a_manufacturer_can_be_created()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('/manufacturers', [
+            'name' => 'Имя производителя',
+            'country' => 'Россия',
+            'dob' => '07.10.1961',
+        ]);
+
+        $manufacturer = Manufacturer::all();
+        $this->assertCount(1, $manufacturer);
+        $this->assertInstanceOf(Carbon::class, $manufacturer->first()->dob);
+        $this->assertEquals('07.10.1961', $manufacturer->first()->dob->format('d.m.Y'));
     }
 }
